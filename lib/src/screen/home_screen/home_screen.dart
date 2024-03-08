6817486bfeb9099fceac93c57318591823c7e97d
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-08 20:53:14 +0700

    feat: Arrange layouts on the welcome_page

--- lib/src/screen/home_screen/home_screen.dart
+++ lib/src/screen/home_screen/home_screen.dart
@@ -10,6 +10,52 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("NarrativeNet"),
+        elevation: 0,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(8),
+          children: [
+            DrawerHeader(
+              child: Image.asset(
+                'assets/image/drawer/narrativenet.png',
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.person),
+                      title: const Text('Profile'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.favorite),
+                      title: const Text('Favorite'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.library_books),
+                      title: const Text('Books'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.book),
+                      title: const Text('Genre'),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
