7d5b9e69d52288051739e7ece4b005e47c625434
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-08 20:51:04 +0700

    feat: reusable widget button and fields

---
+++ lib/src/common/active_button/buttons.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ActionButtons extends StatelessWidget {
+  const ActionButtons({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  }) : isText = false;
+
+  const ActionButtons.text({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  }) : isText = true;
+
+  final VoidCallback onPressed;
+  final String text;
+  final bool isText;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor:
+            isText ? Colors.transparent : Theme.of(context).colorScheme.primary,
+        foregroundColor: isText
+            ? Theme.of(context).colorScheme.onSurface
+            : Theme.of(context).colorScheme.onPrimary,
+        textStyle: Theme.of(context).textTheme.labelLarge,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: const EdgeInsets.all(16),
+        shadowColor: isText ? Colors.transparent : null,
+        surfaceTintColor: isText ? Colors.transparent : null,
+      ),
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
---
+++ lib/src/common/fields/text_input_fields.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class TextInputFields extends StatefulWidget {
+  const TextInputFields({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.validator,
+    this.keyboardType = TextInputType.name,
+  }) : isPassword = false;
+
+  const TextInputFields.password({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.validator,
+    this.keyboardType = TextInputType.name,
+  }) : isPassword = true;
+
+  final TextEditingController controller;
+  final bool isPassword;
+  final String hintText;
+  final TextInputType keyboardType;
+  final String Function(String?)? validator;
+  @override
+  State<TextInputFields> createState() => _TextInputFieldsState();
+}
+
+class _TextInputFieldsState extends State<TextInputFields> {
+  bool isVisible = false;
+
+  void onToggleVisibility() {
+    setState(() {
+      isVisible = !isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      decoration: InputDecoration(
+        suffixIcon: widget.isPassword
+            ? GestureDetector(
+                onTap: onToggleVisibility,
+                child: Icon(
+                  isVisible ? Icons.visibility : Icons.visibility_off,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              )
+            : null,
+        hintText: widget.hintText,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary,
+            width: 2,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.error,
+            width: 2,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.error,
+            width: 2,
+          ),
+        ),
+        filled: true,
+        fillColor:
+            Theme.of(context).colorScheme.inversePrimary.withOpacity(0.4),
+        contentPadding: const EdgeInsets.all(16),
+      ),
+      obscureText: widget.isPassword && !isVisible,
+      validator: widget.validator,
+    );
+  }
+}
