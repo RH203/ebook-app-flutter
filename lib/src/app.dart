cc60742455dc9ff9d68b8acc5160e80b4706b878
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-11 00:29:27 +0700

    feat: change theme intoSplit a theme into different files

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,51 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:novel_app/src/provider/theme/provider_theme.dart';
 import 'package:novel_app/src/screen/auth/login_screen.dart';
 import 'package:novel_app/src/screen/auth/signup_screen.dart';
 import 'package:novel_app/src/screen/home_screen/home_screen.dart';
 import 'package:novel_app/src/screen/profile/profile_screen.dart';
 import 'package:novel_app/src/screen/welcome_screen/welcome_screen.dart';
+import 'package:novel_app/src/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "E-book app",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1F41BB),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          ),
-          titleLarge: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-          bodyLarge: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),
-          labelLarge: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        fontFamily: GoogleFonts.poppins().fontFamily,
-        useMaterial3: true,
+    return Consumer<ProviderTheme>(
+      builder: (context, value, child) => MaterialApp(
+        title: "E-book app",
+        theme: CustomTheme.themeData(value.darkTheme, context),
+        routes: {
+          '/loginscreen': (context) => const LoginScreen(),
+          '/signupscreen': (context) => const SignupScreen(),
+          '/mainscreen': (context) => const HomeScreen(),
+          '/profilscreen': (context) => const ProfileScreen(),
+        },
+        home: const WelcomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      routes: {
-        '/loginscreen': (context) => const LoginScreen(),
-        '/signupscreen': (context) => const SignupScreen(),
-        '/mainscreen': (context) => const HomeScreen(),
-        '/profilscreen': (context) => const ProfileScreen(),
-      },
-      home: const WelcomeScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
