462e692b98c7b48286ea8579f5975ab7ffc64b80
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-11 00:18:41 +0700

    feat: init ProviderTheme in root file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:novel_app/src/app.dart';
+import 'package:novel_app/src/provider/theme/provider_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ProviderTheme(),
+      child: const App(),
+    ),
+  );
 }
