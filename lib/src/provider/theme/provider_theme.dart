49349ca6db68f6c3cb4f47a63428862b6f7e0233
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-11 00:22:00 +0700

    feat: setup provider for dark mode and light mode

---
+++ lib/src/provider/theme/provider_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:novel_app/src/shared_preferences/theme_preferences.dart';
+
+class ProviderTheme extends ChangeNotifier {
+  ThemePreferences themePreferences = ThemePreferences();
+  bool _darkTheme = false;
+
+  bool get darkTheme => _darkTheme;
+
+  bool setDarkTheme() {
+    return _darkTheme;
+  }
+
+  void toggleTheme() {
+    _darkTheme = !_darkTheme;
+    themePreferences.setDarkTheme(_darkTheme);
+    notifyListeners();
+  }
+}
