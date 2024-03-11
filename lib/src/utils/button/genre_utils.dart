f6bdf887e21abcce4f56f15bd417fcd09d0ff107
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-12 00:47:12 +0700

    feat: add temp title for genre Button

---
+++ lib/src/utils/button/genre_utils.dart
@@ -0,0 +1,36 @@
+class GenreUtils {
+  final List<String> _genreTitle = [
+    "Fantasy",
+    "Science Fiction",
+    "Mystery",
+    "Romance",
+    "Thriller",
+    "Horror",
+    "Adventure",
+    "Drama",
+    "Historical Fiction",
+    "Comedy",
+    "Action",
+    "Pirate Art",
+    "Short Story",
+    "Scientific",
+    "Dystopian",
+    "Supernatural",
+    "Children's Story",
+    "Adult Fiction",
+    "Inspirational",
+    "Mythology",
+    "Mystical",
+    "Picture Story",
+    "Classic",
+    "Scientific",
+    "Superhero",
+    "War",
+    "Spiritual",
+    "Psychological",
+    "Serial Story",
+    "Politics",
+  ];
+
+  List<String> get genreTitle => _genreTitle;
+}
