8db819ec7d7c16ce36b1d0fdb9aaab0403d9016e
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-13 10:18:33 +0700

    feat: init screen favorite_screen

---
+++ lib/src/screen/favorite_screen/favorite_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(OctIcons.arrow_left),
+          onPressed: () => Navigator.popAndPushNamed(context, '/mainscreen'),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: const Column(
+          children: [
+            Center(
+              child: Text("Favorite Screen"),
+            )
+          ],
+        ),
+      ),
+    );
+    ;
+  }
+}
