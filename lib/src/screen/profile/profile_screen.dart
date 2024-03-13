ca8406732134a2097d2ad963d6f18588d2ab74a8
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-03-13 10:19:41 +0700

    feat: init screen profile_screen

--- lib/src/screen/profile/profile_screen.dart
+++ lib/src/screen/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +11,24 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(OctIcons.arrow_left),
+          onPressed: () => Navigator.popAndPushNamed(context, '/mainscreen'),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: const Column(
+          children: [
+            Center(
+              child: Text("Profile Screen"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
